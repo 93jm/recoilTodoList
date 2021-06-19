8732dc9e159beddc8aa7a1db64053c00dc2b626b
93jm <65124480+93jm@users.noreply.github.com>
2021-06-20 02:52:52 +0900

    recoil TodoList Practice

---
+++ src/RecoilTodoList/TodoItem.js
@@ -0,0 +1,54 @@
+import { useRecoilState } from "recoil";
+import { todoListState } from "../Recoil/atom";
+
+export default function TodoItem({ item }) {
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+  const index = todoList.findIndex((listItem) => listItem === item);
+
+  const editItemText = ({ target: { value } }) => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      text: value,
+    });
+    console.log("newList >> ", newList);
+    setTodoList(newList);
+  };
+
+  const toggleItemCompletion = () => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      isComplete: !item.isComplete,
+    });
+
+    setTodoList(newList);
+  };
+
+  const deleteItem = () => {
+    const newList = removeItemAtIndex(todoList, index);
+
+    setTodoList(newList);
+  };
+
+  return (
+    <div>
+      <input type="text" value={item.text} onChange={editItemText} />
+      <input
+        type="checkbox"
+        checked={item.isComplete}
+        onChange={toggleItemCompletion}
+      />
+      <button onClick={deleteItem}>X</button>
+    </div>
+  );
+}
+
+function replaceItemAtIndex(arr, index, newValue) {
+  console.log("arr은 ? >>", arr);
+  console.log("index는 ? >>>", index);
+  console.log("newValue는 ? >>>", newValue);
+  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
+}
+
+function removeItemAtIndex(arr, index) {
+  return [...arr.slice(0, index), ...arr.slice(index + 1)];
+}
